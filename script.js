7023b92b95f71c3f9d3bd02d4a78b17af3be2799
Brandon Castro <castroyou999@gmail.com>
2022-07-26 21:44:53 -0400

    changed css background color, added demo button

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript, I'm learning web development";
 document.querySelector("body").appendChild(h2);
+
+const inputButton = document.getElementById('button');
+function clickDemo() {
+    alert("This button doesn't seem to do anything.");
+};
+inputButton.addEventListener('click', clickDemo)
+
